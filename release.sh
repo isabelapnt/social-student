6d4e8aa199d103ed0feaa6e6f58328243b35c95c
Isabela Pinto <isabelasouzapinto@gmail.com>
2018-02-03 19:24:49 -0300

    flush db

--- release.sh
+++ release.sh
@@ -6,11 +6,11 @@ python manage.py migrate --noinput
 
 python manage.py collectstatic --noinput
 
-# python manage.py flush --noinput
-# python manage.py fix_super_user 
-# python manage.py fix_unidade_curso 
-# python manage.py fix_tag 
-# python manage.py fix_servico 
-# python manage.py fix_usuarios 
-# python manage.py fix_post 
+python manage.py flush --noinput
+python manage.py fix_super_user 
+python manage.py fix_unidade_curso 
+python manage.py fix_tag 
+python manage.py fix_servico 
+python manage.py fix_usuarios 
+python manage.py fix_post 
 
