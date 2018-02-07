969a9b46cc1e8e82e46b6d7810a07cd2ffe16c88
Isabela Pinto <isabelasouzapinto@gmail.com>
2018-02-06 22:34:36 -0300

    update release.sh

--- project/settings.py
+++ project/settings.py
@@ -68,6 +68,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'social_django.middleware.SocialAuthExceptionMiddleware',
     'core.rh.middleware.user_type.UserTypeMiddleware',
